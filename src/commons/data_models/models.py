6f86315024511bc866dd1bf6c35dfcf875038202
mard4 <martina.dangelo@live.it>
2025-11-19 02:03:11 +0100

    fixes

--- src/commons/data_models/models.py
+++ src/commons/data_models/models.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 class MongoDBConfig(BaseModel):
     connection_string: str = Field(os.getenv("MONGODB_CONNECTION_STRING", "mongodb://localhost:27017/"))
-    database_name: str = Field(os.getenv("MONGODB_DATABASE_NAME", "fsd50k_db"))
+    database_name: str = Field(os.getenv("MONGODB_DATABASE_NAME", "audio_db"))
     audio_collection: str = Field(os.getenv("MONGODB_AUDIO_COLLECTION", "audio_samples"))
     fs_collection: str = Field(os.getenv("MONGODB_FS_COLLECTION", "audio_files"))
--- src/data_retrievial/reddit/reddit.py
+++ src/data_retrievial/reddit/reddit.py
@@ -1,13 +1,9 @@
 import praw
 import pandas as pd
-import load_env
+from dotenv import load_dotenv
 import os
 
-reddit = praw.Reddit(
-    client_id="sYWC73tdOG1rl-55VQsIvw",
-    client_secret="LZFYc2VRLSht2YSpD-t7JXgzI9Heuw",
-    user_agent="prova by /u/helig0"
-)
+load_dotenv()
 
 def searchByTopic(subreddit, query, query_terms):
   """search topic in Subreddit"""
@@ -35,7 +31,6 @@ df = pd.DataFrame(posts)
 print(df)
 
 if __name__ == "__main__":
-    load_env.load_env()
     
     reddit = praw.Reddit(client_id = os.environ['REDDIT_CLIENT_ID'],
                             client_secret = os.environ['REDDIT_CLIENT_SECRET'],
